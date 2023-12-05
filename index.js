c24ceefc7108db2928208a84b858ff94f350141b
shoshana-solomyak <shoshanasolomyak@hilma.tech>
2023-12-05 12:41:59 +0200

    to pull

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
-<<<<<<< HEAD
-var mysql = require("mysql");
+const mysql = require("mysql");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "z10mz10m",
   database: "schoolData",
 });
 
+// adding admins
+
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE schoolData", function (err, result) {
+});
+
+con.query(
+  "INSERT INTO admin (name, password, school_id) VALUES ?",
+  [
+    [
+      ["shoshana", "123", 1],
+      ["shoham", "456", 2],
+    ],
+  ],
+  function (err, result) {
     if (err) throw err;
-    console.log("Database created");
-  });
-});
-
-function createTable() {}
-=======
-const mysql = require('mysql');
-const router = express.Router()
-const fs = require('fs');
-const express = require('express');
-const app = express();
-const path = require('path')
-
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "z10mz10m",
-    database: "schoolData"
-});
-
-//adding admins
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-
-// con.query("INSERT INTO admin (name, password, school_id) VALUES ?", [[['shoshana', '123', 1], ['shoham', '456', 2]]], function (err, result) {
-//     if (err) throw err;
-//     console.log("admin inserted");
-// })
+    console.log("admin inserted");
+  }
+);
 
 router.listen(8080, () => {
-    console.log(`listening on port 8080`);
-})
->>>>>>> f6d48644f93fe9d99b7ae6be798fc4f81ff08fc5
+  console.log(`listening on port 8080`);
+});
